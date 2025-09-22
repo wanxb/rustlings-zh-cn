013d555a1f9668bef27660ede84e976045fb7e88
wangxunbing <wangxunbing@yaodaitong.com.cn>
2025-09-22 14:47:42 +0800

    finish 10-12

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -4,6 +4,11 @@
 // 如果 `hour_of_day` 大于23，则返回 `None`。
 fn maybe_icecream(hour_of_day: u16) -> Option<u16> {
     // TODO: 完成函数体。
+    match hour_of_day {
+        0..=21 => Some(5),
+        22..=23 => Some(0),
+        _ => None,
+    }
 }
 
 fn main() {
@@ -17,7 +22,7 @@ mod tests {
     #[test]
     fn raw_value() {
         // TODO: 修复此测试。如何获取 `Option` 中包含的值？
-        let icecreams = maybe_icecream(12);
+        let icecreams = maybe_icecream(12).unwrap();
 
         assert_eq!(icecreams, 5); // 不要修改此行代码。
     }
